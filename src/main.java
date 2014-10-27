3c5624a88fffaa4248132d4fc46c847b705a8bb4
gar13177 <gar13177@uvg.edu.gt>
2014-10-26 18:06:26 -0600

    Inicio

---
+++ src/main.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author Kevin
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
